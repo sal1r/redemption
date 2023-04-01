0adcd9bb6fd3821ab731428164997b81d0c4990b
holu31 <coepop31@gmail.com>
2023-04-01 16:59:01 +0300

    Перепутал

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -1,3 +1,7 @@
 extends Node2D
 
 @onready var camera = $MainCamera
+
+func _input(event):
+	if event.is_action_pressed("character_change"):
+		GameManager.target = !GameManager.target
--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -1,7 +1,3 @@
 extends Node
 
 var target: bool = false
-
-func _input(event):
-	if event.is_action_pressed("character_change"):
-		GameManager.target = !GameManager.target
