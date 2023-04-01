b94834ed38a8356b6f46981f79f34ddea1f412d0
sal1r <sal1r57@yandex.ru>
2023-04-01 21:10:00 +0300

    Добавил плавный переход из меню

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -3,16 +3,23 @@ extends Control
 @onready var play = $Center/Buttons/Play
 @onready var authors = $Center/Buttons/Authors
 @onready var exit = $Center/Buttons/Exit
+@onready var transition_rect = $TransitionsRect
+var transition = false
+var transition_stage = 0
 
-
+func _physics_process(delta):
+	if transition:
+		transition_stage += 0.02
+		transition_rect.color.a = transition_stage
+		if transition_stage >= 1:
+			GameManager.load_game()
 
 func _on_play_pressed():
-	GameManager.load_game()
-
+	transition = true
+	transition_rect.visible = true
 
 func _on_authors_pressed():
 	pass # Replace with function body.
-
 
 func _on_exit_pressed():
 	GameManager.exit()
