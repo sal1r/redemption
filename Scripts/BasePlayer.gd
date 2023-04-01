5d72e2756eb922153b3d39128eda3f2a557a92e9
holu31 <coepop31@gmail.com>
2023-04-01 16:05:19 +0300

    Код немного покороче

--- Scripts/BasePlayer.gd
+++ Scripts/BasePlayer.gd
@@ -2,25 +2,15 @@ extends CharacterBody2D
 class_name Player
 
 var speed = 500
-var dir = Vector2.ZERO
+var direction = Vector2.ZERO
 
 func walk():
-	if Input.is_action_just_pressed("left"):
-		dir.x = -1
-	if Input.is_action_just_pressed("right"):
-		dir.x = 1
-	if Input.is_action_just_pressed("up"):
-		dir.y = -1
-	if Input.is_action_just_pressed("down"):
-		dir.y = 1
-	
-	if !(Input.is_action_pressed("left")) and !(Input.is_action_pressed("right")):
-		dir.x = 0
-	
-	if !(Input.is_action_pressed("up")) and !(Input.is_action_pressed("down")):
-		dir.y = 0
-	
-	velocity = dir.normalized() * speed
+	direction = Input.get_vector("left", "right", "up", "down")
+
+	velocity = direction.normalized() * speed
+
+	if direction:
+		pass # walk
 
 func _physics_process(delta):
 	walk()
