619b075fcd4f37b36baf96f52f62d51e56427db7
holu31 <coepop31@gmail.com>
2023-04-02 21:53:46 +0300

    двери

---
+++ Scripts/Door.gd
@@ -0,0 +1,12 @@
+extends StaticBody2D
+
+var door_opened: bool = false
+
+func _on_trigger_body_entered(body):
+	if body is Player:
+		if door_opened: return
+		if body.item == GameManager.res.KEY:
+			$CollisionShape2D.queue_free()
+			$CloseDoor.hide()
+			$OpenDoor.show()
+			door_opened = true
