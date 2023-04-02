06e832c59615728351ba9340c6ca55510c3b56ec
sal1r <sal1r57@yandex.ru>
2023-04-03 01:25:15 +0300

    дверь на уровне и багфикс

--- Scripts/Door.gd
+++ Scripts/Door.gd
@@ -1,13 +1,13 @@
 extends StaticBody2D
 
-var door_opened: bool = false
+var opened: bool = false
 
 func _on_trigger_body_entered(body):
 	if body is Player:
-		if door_opened: return
+		if opened: return
 		if body.item == GameManager.res.KEY:
 			$CollisionShape2D.queue_free()
 			$CloseDoor.hide()
 			$OpenDoor.show()
 			body.item = GameManager.res.NONE
-			door_opened = true
+			opened = true
