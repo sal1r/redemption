937e42febef10674dfe7ba17ed14be62d7bbac41
sal1r <sal1r57@yandex.ru>
2023-04-03 19:42:47 +0300

    ДАААААААААААА

--- Scripts/PreEndTrigger.gd
+++ Scripts/PreEndTrigger.gd
@@ -1,8 +1,19 @@
 extends Node2D
 
+@onready var background = get_node("/root/Game/Background/Parallax")
+@onready var particles = get_node("/root/Game/MainCamera/Particles")
+var transition = false
+var transition_stage = 1
 
 func _on_trigger_body_entered(body):
 	if body is Player:
-		get_node("/root/Game/Background").visible = false
-		get_node("/root/Game/MainCamera/Particles").visible = false
+		transition = true
 		GameManager.stop_music()
+
+func _physics_process(delta):
+	if transition:
+		transition_stage -= 0.02
+		background.modulate.a = transition_stage
+		particles.modulate.a = transition_stage
+		if transition_stage <= 0:
+			transition = false
