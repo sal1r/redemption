4b362ac6ed8fe4d53ff622315cb8e0784cb5c579
sal1r <sal1r57@yandex.ru>
2023-04-03 19:14:02 +0300

    почти готово

--- Scripts/Cat.gd
+++ Scripts/Cat.gd
@@ -7,7 +7,13 @@ func _physics_process(delta):
 	if game.target:
 		walk()
 		if direction:
-			anim.flip_h = true if direction.x > 0 else false
+			if direction.x:
+				anim.play("walk_side")
+				anim.flip_h = true if direction.x > 0 else false
+			elif direction.y < 0:
+				anim.play("walk_back")
+			else:
+				anim.play("walk_front")
 		else:
 			anim.play("idle")
 		move_and_slide()
